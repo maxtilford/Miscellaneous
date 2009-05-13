3898839d4ea0fb91b4893df6c3e672795c7c7312
Max Tilford <maxtilford@gmail.com>
2009-05-12 23:13:05 -0700

    fibonnaci by rounding

--- fibonacci.scm
+++ fibonacci.scm
@@ -2,11 +2,16 @@
 
 (define phi (/ (+ 1 (sqrt 5)) 2))
 
-(define (fib n) 
-  (round (/ (- (expt phi n) 
+(define (closed-form n) 
+  (/ (- (expt phi n) 
 	       (expt (- 1 phi) n)) 
-	    (sqrt 5))))
+	    (sqrt 5)))
 
-(print (fib (read)))
+(define (fib-by-rounding n)
+  (floor (+ (/ (expt phi n) 
+	       (sqrt 5)) 
+	    (/ 1 2))))
+
+(print (fib-by-rounding (read)))
 
 
